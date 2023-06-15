f35ceaea5e37c58da635eedb52701c6febb50d89
Noelia <perezgonzaleznoelia08@gmail.com>
2023-06-15 12:53:15 +0200

    mmmmmmm

---
+++ proyectoFinalNoelia/src/main/java/persistensias/cambioPersistencia.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package persistensias;
+
+/**
+ *
+ * @author noelia
+ */
+public class cambioPersistencia {
+    
+    /*
+        PERSISTENCIAS CASITA UWU
+      <property name="javax.persistence.jdbc.url" value="jdbc:mysql://localhost:3306/artebd?zeroDateTimeBehavior=CONVERT_TO_NULL"/>
+      <property name="javax.persistence.jdbc.user" value="root"/>
+      <property name="javax.persistence.jdbc.driver" value="com.mysql.cj.jdbc.Driver"/>
+      <property name="javax.persistence.jdbc.password" value="8313"/>
+    */
+    
+    /*
+        PERSISTENCIAS COLE UWU 
+    
+      <property name="javax.persistence.jdbc.url" value="jdbc:mysql://192.168.56.101/ArteBD?zeroDateTimeBehavior=CONVERT_TO_NULL"/>
+      <property name="javax.persistence.jdbc.user" value="noelia"/>
+      <property name="javax.persistence.jdbc.driver" value="com.mysql.cj.jdbc.Driver"/>
+      <property name="javax.persistence.jdbc.password" value="8313"/>
+    
+    
+    */
+    
+}
